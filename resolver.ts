a0b1f1bf10aad7180563b6c100c58a75aa73919c
Yufan Sheng <syhily@gmail.com>
2025-01-08 12:27:13 +0800

    feat: new netease player to use in astro.

---
+++ resolver.ts
@@ -0,0 +1,49 @@
+import { getSongInfo, getSongUrl } from "./netease/song";
+
+export type Song = {
+	name: string;
+	artist: string;
+	url: string;
+	pic: string;
+};
+
+// The props for music player. We support both netease music and direct linked music.
+export interface MusicPlayerProps {
+	netease?: number;
+	song?: Song;
+}
+
+const emptySong = { name: "", artist: "", url: "", pic: "" };
+
+const song = async (props: MusicPlayerProps): Promise<Song> => {
+	const { netease, song } = props;
+
+	if (netease) {
+		const info = await getSongInfo(netease);
+		const url = await getSongUrl(netease);
+
+		// Check the return result.
+		return {
+			name: info[0].title,
+			artist: info[0].author,
+			url: url,
+			pic: info[0].pic,
+		};
+	}
+
+	if (song) {
+		return song;
+	}
+
+	console.error("No song information is provided, check your code.");
+	return emptySong;
+};
+
+export const resolveSong = async (props: MusicPlayerProps): Promise<Song> => {
+	// Try-catch for avoiding the unexpected errors.
+	try {
+		return await song(props);
+	} catch (e) {
+		return emptySong;
+	}
+};
