1d97386726a0d7860885cd85d4da1ba8acf1e3e4
Yufan Sheng <syhily@gmail.com>
2025-01-08 18:01:51 +0800

    chore: reformat the code with better style.

--- netease/encrypt.ts
+++ netease/encrypt.ts
@@ -1,87 +1,75 @@
-import type { HttpMethod } from "./config";
-import { Buffer } from "node:buffer";
-import crypto from "node:crypto";
+import { Buffer } from 'node:buffer';
+import crypto from 'node:crypto';
+import type { HttpMethod } from './config';
 
-const iv = Buffer.from("0102030405060708");
-const presetKey = Buffer.from("0CoJUm6Qyw8W8jud");
-const linuxapiKey = Buffer.from("rFgB&h#%2?^eDg:Q");
-const base62 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+const iv = Buffer.from('0102030405060708');
+const presetKey = Buffer.from('0CoJUm6Qyw8W8jud');
+const linuxapiKey = Buffer.from('rFgB&h#%2?^eDg:Q');
+const base62 = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 const publicKey =
-	"-----BEGIN PUBLIC KEY-----\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDgtQn2JZ34ZC28NWYpAUd98iZ37BUrX/aKzmFbt7clFSs6sXqHauqKWqdtLkF2KexO40H1YTX8z2lSgBBOAxLsvaklV8k4cBFK9snQXE9/DDaFt6Rr7iVZMldczhC0JNgTz+SHXT6CBHuX3e9SdB1Ua44oncaTWz7OBGLbCiK45wIDAQAB\n-----END PUBLIC KEY-----";
-const eapiKey = "e82ckenh8dichen8";
+  '-----BEGIN PUBLIC KEY-----\nMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDgtQn2JZ34ZC28NWYpAUd98iZ37BUrX/aKzmFbt7clFSs6sXqHauqKWqdtLkF2KexO40H1YTX8z2lSgBBOAxLsvaklV8k4cBFK9snQXE9/DDaFt6Rr7iVZMldczhC0JNgTz+SHXT6CBHuX3e9SdB1Ua44oncaTWz7OBGLbCiK45wIDAQAB\n-----END PUBLIC KEY-----';
+const eapiKey = 'e82ckenh8dichen8';
 
 const aesEncrypt = (
-	buffer: Buffer | Uint8Array,
-	mode: string,
-	key: Buffer | Uint8Array | string,
-	iv: Buffer | string,
+  buffer: Buffer | Uint8Array,
+  mode: string,
+  key: Buffer | Uint8Array | string,
+  iv: Buffer | string,
 ) => {
-	const cipher = crypto.createCipheriv(`aes-128-${mode}`, key, iv);
-	return Buffer.concat([cipher.update(buffer), cipher.final()]);
+  const cipher = crypto.createCipheriv(`aes-128-${mode}`, key, iv);
+  return Buffer.concat([cipher.update(buffer), cipher.final()]);
 };
 
 const rsaEncrypt = (buffer: Buffer | Uint8Array, key: string) => {
-	return crypto.publicEncrypt(
-		{ key: key, padding: crypto.constants.RSA_NO_PADDING },
-		Buffer.concat([Buffer.alloc(128 - buffer.length), buffer]),
-	);
+  return crypto.publicEncrypt(
+    { key: key, padding: crypto.constants.RSA_NO_PADDING },
+    Buffer.concat([Buffer.alloc(128 - buffer.length), buffer]),
+  );
 };
 
 export const weapi = (form: Record<string, string>): Record<string, string> => {
-	const text = JSON.stringify(form);
-	const secretKey = crypto
-		.randomBytes(16)
-		.map((n) => base62.charAt(n % 62).charCodeAt(0));
+  const text = JSON.stringify(form);
+  const secretKey = crypto.randomBytes(16).map((n) => base62.charAt(n % 62).charCodeAt(0));
 
-	return {
-		params: aesEncrypt(
-			Buffer.from(
-				aesEncrypt(Buffer.from(text), "cbc", presetKey, iv).toString("base64"),
-			),
-			"cbc",
-			secretKey,
-			iv,
-		).toString("base64"),
-		encSecKey: rsaEncrypt(secretKey.reverse(), publicKey).toString("hex"),
-	};
+  return {
+    params: aesEncrypt(
+      Buffer.from(aesEncrypt(Buffer.from(text), 'cbc', presetKey, iv).toString('base64')),
+      'cbc',
+      secretKey,
+      iv,
+    ).toString('base64'),
+    encSecKey: rsaEncrypt(secretKey.reverse(), publicKey).toString('hex'),
+  };
 };
 
-export const linuxapi = (
-	method: HttpMethod,
-	url: string,
-	form: Record<string, string>,
-): Record<string, string> => {
-	const request = {
-		method: method,
-		url: url.replace(/\w*api/, "api"),
-		params: form,
-	};
+export const linuxapi = (method: HttpMethod, url: string, form: Record<string, string>): Record<string, string> => {
+  const request = {
+    method: method,
+    url: url.replace(/\w*api/, 'api'),
+    params: form,
+  };
 
-	const text = JSON.stringify(request);
-	return {
-		eparams: aesEncrypt(Buffer.from(text), "ecb", linuxapiKey, "")
-			.toString("hex")
-			.toUpperCase(),
-	};
+  const text = JSON.stringify(request);
+  return {
+    eparams: aesEncrypt(Buffer.from(text), 'ecb', linuxapiKey, '').toString('hex').toUpperCase(),
+  };
 };
 
 export const eapi = (
-	url: string,
-	form: Record<string, string>,
-	header: Record<string, string | boolean | number>,
+  url: string,
+  form: Record<string, string>,
+  header: Record<string, string | boolean | number>,
 ): Record<string, string> => {
-	const request = {
-		...form,
-		header: header,
-	};
+  const request = {
+    ...form,
+    header: header,
+  };
 
-	const text = JSON.stringify(request);
-	const message = `nobody${url}use${text}md5forencrypt`;
-	const digest = crypto.createHash("md5").update(message).digest("hex");
-	const data = `${url}-36cd479b6b5-${text}-36cd479b6b5-${digest}`;
-	return {
-		params: aesEncrypt(Buffer.from(data), "ecb", eapiKey, "")
-			.toString("hex")
-			.toUpperCase(),
-	};
+  const text = JSON.stringify(request);
+  const message = `nobody${url}use${text}md5forencrypt`;
+  const digest = crypto.createHash('md5').update(message).digest('hex');
+  const data = `${url}-36cd479b6b5-${text}-36cd479b6b5-${digest}`;
+  return {
+    params: aesEncrypt(Buffer.from(data), 'ecb', eapiKey, '').toString('hex').toUpperCase(),
+  };
 };
--- netease/song.ts
+++ netease/song.ts
@@ -1,100 +1,87 @@
-import { request } from "./request";
+import { request } from './request';
 
 type SongUrl = {
-	url?: string;
+  url?: string;
 };
 
 type SongUrlResponse = {
-	data: SongUrl[];
+  data: SongUrl[];
 };
 
 export interface SongArtist {
-	albumSize: number;
-	id: number;
-	img1v1Id: number;
-	img1v1Url: string;
-	musicSize: number;
-	name: string;
-	picId: number;
-	picUrl: string;
-	topicPerson: number;
-	alias?: string[];
+  albumSize: number;
+  id: number;
+  img1v1Id: number;
+  img1v1Url: string;
+  musicSize: number;
+  name: string;
+  picId: number;
+  picUrl: string;
+  topicPerson: number;
+  alias?: string[];
 }
 
 type SongInfo = {
-	al: {
-		id: number;
-		name: string;
-		picUrl: string;
-	};
-	ar: SongArtist[];
-	dt: number;
-	id: number;
-	name: string;
-	publishTime: number;
-	fee?: number;
-	status?: number;
+  al: {
+    id: number;
+    name: string;
+    picUrl: string;
+  };
+  ar: SongArtist[];
+  dt: number;
+  id: number;
+  name: string;
+  publishTime: number;
+  fee?: number;
+  status?: number;
 };
 
 type SongInfoResponse = {
-	songs?: SongInfo[];
+  songs?: SongInfo[];
 };
 
 export const getSongUrl = async (id: number) => {
-	const data = {
-		ids: `[${id}]`,
-		level: "standard",
-		encodeType: "flac",
-	};
+  const data = {
+    ids: `[${id}]`,
+    level: 'standard',
+    encodeType: 'flac',
+  };
 
-	const res = await request(
-		"POST",
-		"https://interface.music.163.com/eapi/song/enhance/player/url/v1",
-		data,
-		{
-			crypto: "eapi",
-			url: "/api/song/enhance/player/url/v1",
-			cookie: {},
-		},
-	);
-	const url = (res as SongUrlResponse).data[0]?.url?.replace(
-		"http://",
-		"https://",
-	);
+  const res = await request('POST', 'https://interface.music.163.com/eapi/song/enhance/player/url/v1', data, {
+    crypto: 'eapi',
+    url: '/api/song/enhance/player/url/v1',
+    cookie: {},
+  });
+  const url = (res as SongUrlResponse).data[0]?.url?.replace('http://', 'https://');
 
-	return url || `https://music.163.com/song/media/outer/url?id=${id}.mp3`;
+  return url || `https://music.163.com/song/media/outer/url?id=${id}.mp3`;
 };
 
 const mapSongList = (songs: SongInfo[]) => {
-	return songs.map((song) => {
-		const artists = song.ar || [];
-		return {
-			title: song.name,
-			author: artists.reduce((i, v) => (i ? `${i} / ` : i) + v.name, ""),
-			pic: song?.al?.picUrl,
-			url: song.id,
-			lrc: song.id,
-		};
-	});
+  return songs.map((song) => {
+    const artists = song.ar || [];
+    return {
+      title: song.name,
+      author: artists.reduce((i, v) => (i ? `${i} / ` : i) + v.name, ''),
+      pic: song?.al?.picUrl,
+      url: song.id,
+      lrc: song.id,
+    };
+  });
 };
 
 export const getSongInfo = async (id: number) => {
-	const ids = [id];
-	const data = {
-		c: `[${ids.map((id) => `{"id":${id}}`).join(",")}]`,
-	};
-	const res = (await request(
-		"POST",
-		"https://music.163.com/api/v3/song/detail",
-		data,
-		{
-			crypto: "weapi",
-		},
-	)) as SongInfoResponse;
+  const ids = [id];
+  const data = {
+    c: `[${ids.map((id) => `{"id":${id}}`).join(',')}]`,
+  };
+  const res = (await request('POST', 'https://music.163.com/api/v3/song/detail', data, {
+    crypto: 'weapi',
+  })) as SongInfoResponse;
 
-	if (!res.songs) {
-		throw res;
-	}
+  if (!res.songs) {
+    throw res;
+  }
 
-	return mapSongList(res.songs);
+  return mapSongList(res.songs);
 };
